505b18728d196f8a9f6480d6abaf866aa7078cad
NikKowPHP <nik.kow@outlook.com>
2025-06-25 08:02:11 +0200

    chore: remove unused and redundant files

--- copy_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Copy the file from the workspace to the destination
-cp .roo/custom_modes.yaml ~/.config/Code/User/globalStorage/rooveterinaryinc.roo-cline/settings/custom_modes.yaml
-cp .roo/custom_modes.yaml ~/Library/Application\ Support/Code/User/globalStorage/rooveterinaryinc.roo-cline/settings/
--- delete_assistance.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -f NEEDS_ASSISTANCE.md
